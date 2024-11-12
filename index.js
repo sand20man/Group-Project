1d7f67461c39740a039e7f3c8bf2158ffb68656b
Coleman Sandy <sandcole@byu.edu>
2024-11-11 22:11:54 -0700

    Co-authored-by: Coleman Sandy <colemansandy@MacBook-Pro-285.local>

---
+++ index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+
+let app = express();
+
+let path = require('path');
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Welcome to SkillShare',
+        featuredCourses: courses.slice(0, 3)
+    });
+});
+
+const knex = require('knex') ({
+    client : 'pg',
+    connection: {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'is403',
+        database: 'groupproject',
+        port: 5432
+    }
+})
+
+app.listen(port, () => console.log('Listening'));
