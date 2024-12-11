2eed0911ed3855c4b1e972626752de87d3a6678b
Daniel Wait <danielwait6492@gmail.com>
2024-12-11 08:13:31 -0700

    i think its done

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const knex = require('knex')({
         user: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
         database: process.env.RDS_DB_NAME,
-        port: process.env.RDS_PORT
+        port: process.env.RDS_PORT,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 });
 
